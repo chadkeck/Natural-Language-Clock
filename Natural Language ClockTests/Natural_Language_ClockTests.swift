702217ece19c825eec7bda73364b48fd78077394
Chad Bibler <chad@chadbibler.com>
2015-08-21 13:34:14 -0500

    38mm UI beginning

---
+++ Natural Language ClockTests/Natural_Language_ClockTests.swift
@@ -0,0 +1,36 @@
+//
+//  Natural_Language_ClockTests.swift
+//  Natural Language ClockTests
+//
+//  Created by Chad Bibler on 8/21/15.
+//  Copyright © 2015 Chad Bibler. All rights reserved.
+//
+
+import XCTest
+@testable import Natural_Language_Clock
+
+class Natural_Language_ClockTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
