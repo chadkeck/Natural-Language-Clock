ee201535817235495474421541d1ec69f211880e
Chad Bibler <chad@chadbibler.com>
2015-08-21 13:42:05 -0500

    Hook label outlets to controller

--- Natural Language Clock WatchKit Extension/InterfaceController.swift
+++ Natural Language Clock WatchKit Extension/InterfaceController.swift
@@ -11,10 +11,33 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    @IBOutlet var itLabel: WKInterfaceLabel!
+    @IBOutlet var isLabel: WKInterfaceLabel!
+    @IBOutlet var halfLabel: WKInterfaceLabel!
+    @IBOutlet var tenMinutesLabel: WKInterfaceLabel!
+    @IBOutlet var quarterLabel: WKInterfaceLabel!
+    @IBOutlet var twentyLabel: WKInterfaceLabel!
+    @IBOutlet var fiveMinutesLabel: WKInterfaceLabel!
+    @IBOutlet var minutesLabel: WKInterfaceLabel!
+    @IBOutlet var toLabel: WKInterfaceLabel!
+    @IBOutlet var pastLabel: WKInterfaceLabel!
+    @IBOutlet var oneLabel: WKInterfaceLabel!
+    @IBOutlet var threeLabel: WKInterfaceLabel!
+    @IBOutlet var twoLabel: WKInterfaceLabel!
+    @IBOutlet var fourLabel: WKInterfaceLabel!
+    @IBOutlet var fiveHourLabel: WKInterfaceLabel!
+    @IBOutlet var sixLabel: WKInterfaceLabel!
+    @IBOutlet var sevenLabel: WKInterfaceLabel!
+    @IBOutlet var eightLabel: WKInterfaceLabel!
+    @IBOutlet var nineLabel: WKInterfaceLabel!
+    @IBOutlet var tenHourLabel: WKInterfaceLabel!
+    @IBOutlet var elevenLabel: WKInterfaceLabel!
+    @IBOutlet var twelveLabel: WKInterfaceLabel!
+    @IBOutlet var oclockLabel: WKInterfaceLabel!
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
+
         // Configure interface objects here.
     }
 
