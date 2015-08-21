75e3d8bbbabe5efb87a1ccf796223f403ed6812e
Chad Bibler <chad@chadbibler.com>
2015-08-21 17:24:39 -0500

    Hook up iOS label outlets

--- Natural Language Clock/ViewController.swift
+++ Natural Language Clock/ViewController.swift
@@ -9,6 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var itLabel: UILabel!
+    @IBOutlet var isLabel: UILabel!
+    @IBOutlet var halfLabel: UILabel!
+    @IBOutlet var tenMinutesLabel: UILabel!
+    @IBOutlet var quarterLabel: UILabel!
+    @IBOutlet var twentyLabel: UILabel!
+    @IBOutlet var fiveMinutesLabel: UILabel!
+    @IBOutlet var minutesLabel: UILabel!
+    @IBOutlet var toLabel: UILabel!
+    @IBOutlet var pastLabel: UILabel!
+    @IBOutlet var oneLabel: UILabel!
+    @IBOutlet var threeLabel: UILabel!
+    @IBOutlet var twoLabel: UILabel!
+    @IBOutlet var fourLabel: UILabel!
+    @IBOutlet var fiveHourLabel: UILabel!
+    @IBOutlet var sixLabel: UILabel!
+    @IBOutlet var sevenLabel: UILabel!
+    @IBOutlet var eightLabel: UILabel!
+    @IBOutlet var nineLabel: UILabel!
+    @IBOutlet var tenHourLabel: UILabel!
+    @IBOutlet var elevenLabel: UILabel!
+    @IBOutlet var twelveLabel: UILabel!
+    @IBOutlet var oclockLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
